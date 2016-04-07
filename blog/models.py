5a4df8ceb45a8379dbed936fad5ecba58d810e92
Jack <z846204062@hotmail.com>
2016-04-07 22:57:01 +0800

    数据库部分更新

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    gender = models.IntegerField()
+    intime = models.DateTimeField()
+
+    class Meta:
+        db_table = 'student'
+
+
+class teacher(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'teacher'
